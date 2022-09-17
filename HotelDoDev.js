66b33eeaf59f07134c66def98596c429e60ddcee
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-09-16 22:21:21 -0300

    Atualizando com as funções para exibir todas as reservas

--- HotelDoDev.js
+++ HotelDoDev.js
@@ -1,13 +1,13 @@
 // HOTEL
 let idHotel = [1, 2, 3]
-let nomesHotel = ["Hotel1", "Hotel2", "Hotel3"]
+let nomesHotel = ["Hotel 1", "Hotel 2", "Hotel 3"]
 let categoria = ["A", "B", "C"]
-let endereco = ["End1", "End2", "End3"]
+let endereco = ["End 1", "End 2", "End 3"]
 let telefone = [222, 555, 333]
 // RESERVA
 let reservaId = [4, 5, 6]
-let reservaIdHotel = [2, 3, 1]
-let reservaNomeResponsavel = ["João ", "Fulano", "Marcos"]
+let reservaIdHotel = [2, 1, 3]
+let reservaNomeResponsavel = ["Fulano", "Marcos", "Marcos"]
 let reservaDiaEntrada = [8, 11, 17]
 let reservaDiaSaida = [11, 16, 25]
 
@@ -16,7 +16,10 @@ let reservaDiaSaida = [11, 16, 25]
 console.log("Dados Hotel -  " + idHotel + "  -  " + nomesHotel + "  -  " + categoria + "  -  " + endereco + "  -  " + telefone)
 console.log("Dados Reserva -  " + reservaId + "  -  " + reservaIdHotel + "  -  " + reservaNomeResponsavel + "  -  " + reservaDiaEntrada + "  -  " + reservaDiaSaida)
 
-ExibirTodasReservas(1)
+// let idBuscar = prompt(`Qual o ID do Hotel que deseja buscar?`)
+// ExibirReservasDoHotel(3)
+// ExibirHotelEnderecoDiaDeEntradaEsaida(5)
+ExibirTodasAsReservas("Marcos")
 
 function CadastrarHotel(){
     id.push(prompt(`Qual o ID do hotel?`))
@@ -25,7 +28,6 @@ function CadastrarHotel(){
     endereco.push(prompt(`Qual o endereço do hotel?`))
     telefone.push(prompt(`Qual o telefone do hotel?`))
 }
-
 function CadastrarReserva(){
     reservaId.push(prompt(`Qual o ID da reserva?`))
     reservaIdHotel.push(prompt(`Qual o ID do hotel?`))
@@ -34,9 +36,30 @@ function CadastrarReserva(){
     reservaDiaSaida.push(prompt(`Qual o dia de saída?`))
 }
 
-function ExibirTodasReservas(idHotelParametro){
-    let posicao = idHotel.indexOf(idHotelParametro)
-    console.log(posicao)    
+function ExibirReservasDoHotel(idHotelParametro){
+    let posicao = idHotel.indexOf(idHotelParametro)     
     console.log(`Nome: ${nomesHotel[posicao]}, Nome do responsável: ${reservaNomeResponsavel[posicao]}, Dia de entrada: ${reservaDiaEntrada[posicao]}, Dia de saída: ${reservaDiaSaida[posicao]}`)
+}
+function ExibirHotelEnderecoDiaDeEntradaEsaida(idReservaParametro){
+    let posicao = reservaId.indexOf(idReservaParametro)     
+    console.log(`Nome: ${nomesHotel[posicao]}, Endereço: ${endereco[posicao]}, Dia de entrada: ${reservaDiaEntrada[posicao]}, Dia de saída: ${reservaDiaSaida[posicao]}`)
+}
+function ExibirTodasAsReservas(nomeParametro){
+    let reservaAux = []
+    let reservaIdHotelAux = []
+    let nomeAux = []
+    let diaEntrada = []
+    let diaSaida = []
+    reservaNomeResponsavel.filter((element, index) => {
+        if(element == nomeParametro){
+            reservaAux.push(reservaId[index])
+            reservaIdHotelAux.push(reservaIdHotel[index])
+            nomeAux.push(reservaNomeResponsavel[index])
+            diaEntrada.push(reservaDiaEntrada[index])
+            diaSaida.push(reservaDiaSaida[index])
+        }          
+    })
+    console.log(`ID Reserva: ${reservaAux}, ID Hotel: ${reservaIdHotelAux}, Nome: ${nomeAux}, Dia Entrada: ${diaEntrada}, Dia Saída ${diaSaida}`)
+    
 }
 
